f49f51679ceb7d82f3c753fb89cc3c5ba9da1037
kanbass <knb1031_ba@icloud.com>
2021-11-17 19:56:29 +0900

    Auth error 対応

--- src/tests/Auth.test.js
+++ src/tests/Auth.test.js
@@ -8,25 +8,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import Auth from "../components/Auth";
 
-const mockHistoryPush = jest.fn();
-
-jest.mock("react-router-dom", () => ({
-    useHistory: () => ({
-        push: mockHistoryPush,
-    }),
-}));
-
-const handlers = [
-    rest.post("http://localhost:8000/api/auth/", (req, res, ctx) => {
-        return res(ctx.status(200), ctx.json({ token: "abc123" }));
-    }),
-    rest.post("http://localhost:8000/api/createuser/", (req, res, ctx) => {
-        return res(ctx.status(201));
-    }),
-];
-
-const server = setupServer(...handlers);
-
 beforeAll(() => {
     server.listen();
 });
@@ -38,7 +19,7 @@ afterAll(() => {
     server.close();
 });
 
-describe("Auth Component Test Cases", () => {
+describe("Sample Test Cases", () => {
     let store;
     beforeEach(() => {
         store = configureStore({
@@ -55,12 +36,5 @@ describe("Auth Component Test Cases", () => {
         );
         // screen.debug(); //取得した画面情報はscreenに保存されている。この文言でscreenの内容を確認することができる。
         expect(true).toBe(true);
-
-        // expect(screen.getByTestId("label-username")).toBeTruthy();
-        // expect(screen.getByTestId("label-password")).toBeTruthy();
-        // expect(screen.getByTestId("input-username")).toBeTruthy();
-        // expect(screen.getByTestId("input-password")).toBeTruthy();
-        // expect(screen.getByRole("button")).toBeTruthy();
-        // expect(screen.getByTestId("toggle-icon")).toBeTruthy();
     });
 });
